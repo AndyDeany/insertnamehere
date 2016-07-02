d4615f91a3414ebf7ea900380dc629c3a821c8fe
AndyDeany <oneandydean@hotmail.com>
2016-07-02 23:17:54 +0100

    Create game screen and start creating other globals

--- insertnamehere.py
+++ insertnamehere.py
@@ -25,15 +25,52 @@ try:    # Importing and initialising pygame
     except Exception, error:
         pass
     pygame.init()
-except Exception, error:
+except Exception as error:
     log("Failed to initialise pygame")
     
 try:    # Importing other modules
     import sys
     import datetime, time
     import math, random
-except Exception, error:
-    log(error, "Failed to import modules")
+except Exception as error:
+    log("Failed to import modules")
 ### ---------- IMPORTING MODULES - END ---------- ###
+
+
+### ---------- INITIALISING GLOBAL VARIABLES ---------- ###
+try:    # Initialising game screen
+    screen = pygame.display.set_mode((0,0), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN)  # (0,0) defaults to the user's screen size
+    screen_width = screen.get_width()
+    screen_height = screen.get_height()
+    MONITOR_WIDTH = screen_width
+    MONITOR_HEIGHT = screen_height
     
-    
+    pygame.display.set_caption("insertnamehere (Alpha 1.0)")
+    #! Insert code for game icon here
+    # Defining a fucntion to change the screen settings after it has been created
+    def reinitialise_screen(resolution=(screen_width,screen_height), mode="fullscreen"):
+        global error
+        try:
+            global screen, screen_width, screen_height
+            screen_width = resolution[0]
+            screen_height = resolution[1]
+            if mode == "fullscreen":
+                screen = pygame.display.set_mode(resolution, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN)
+            elif mode == "windowed":
+                os.environ['SDL_VIDEO_WINDOW_POS'] = "".join(str((MONITOR_WIDTH - screen_width)/2), ",", str((MONITOR_HEIGHT - screen_height)/2))
+                screen = pygame.display.set_mode(resolution, pygame.HWSURFACE|pygame.DOUBLEBUF)
+            elif mode == "borderless":
+                os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
+                screen = pygame.display.set_mode(resolution, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.NOFRAME)
+            else:
+                error = "Unknown mode for reinitialise_screen(): \"" + mode + "\" [syntax error]."
+                raise
+        except Exception, error:
+            log("Failed to reinitialise screen in " + mode + " mode at " + str(screen_width) + "x" + str(screen_height) + " resolution")
+except Exception as error:
+    log("Failed to initialise game screen")
+
+try:    # Initialising other variables
+    fps = 60
+except Exception as error:
+    log("Failed to initialise other global variables")
